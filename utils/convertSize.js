d45d57056b2b03507dfc7be205be0e6df0a1b15f
Abdullahaly <abdullahaly.dev@gmail.com>
2023-04-03 03:12:37 +0200

    deleted some utils

--- utils/convertSize.js
+++
@@ -1,8 +0,0 @@
-function convertSize(bytes) {
-    var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', "PB", "EB", "ZB", "YB"];
-    if (bytes == 0) return "0 MB";
-    var i = parseInt(Math.floor(Math.log(bytes) / Math.log(1024)));
-    if (i == 0) return bytes + ' ' + sizes[i];
-    return (bytes / Math.pow(1024, i)).toFixed(1) + ' ' + sizes[i];
-};
-module.exports = convertSize
